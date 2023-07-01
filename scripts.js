62822f2a243b8b5d6609539a5708ccaabc70f2df
Alex Pushkin <apuskin@netinfo.md>
2023-07-01 12:41:47 +0300

    Add responsiveness

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-70px";
+    document.getElementById("navbar").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
